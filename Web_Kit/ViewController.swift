ea5eaa0329087d67a731558f627ad302121b99da
Dmitriy Danilin <danilindn@icloud.com>
2020-07-21 18:17:01 +0300

    Initial Commit

---
+++ Web_Kit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Web_Kit
+//
+//  Created by Дмитрий Данилин on 21.07.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
