5f591de601c3fd4544e10affc61230302d9673bf
Dmitriy Danilin <danilindn@icloud.com>
2020-07-21 20:11:58 +0300

    Окончание урока 18

--- Web_Kit/ViewController.swift
+++ Web_Kit/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forvardButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        urlTextField.delegate = self
+        webView.navigationDelegate = self
+        
+        let homePage = "https://zyfunphoto.ru"
+        let url = URL(string: homePage)
+        let request = URLRequest(url: url!)
+        
+        urlTextField.text = homePage
+        
+        webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true
+        
     }
-
+    @IBAction func backButtonAction(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    @IBAction func forwardButtonAction(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
 
 }
 
+extension ViewController: UITextFieldDelegate, WKNavigationDelegate  {
+    // Метод позволяет вернуть данные из текстовой строки
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Присваиваем константе значение, которое пользователь вводит в текстовую строку
+        let urlString = urlTextField.text!
+        // Переводим это значение в URL
+        let url = URL(string: urlString)!
+        // Передаём значение в request
+        let request = URLRequest(url: url)
+        
+        // Загружаем переданные данные
+        webView.load(request)
+        
+        // Скрываем клавиатуру
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+    // Метод позволяет перенести адрес текущей страницы в TextField
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // Присваиваем значение текущей страницы полу ввода текста
+        urlTextField.text = webView.url?.absoluteString
+        
+        // Делаем доступной кнопку назад и вперед, если был переход на странице
+        backButton.isEnabled = webView.canGoBack
+        forvardButton.isEnabled = webView.canGoForward
+    }
+}
